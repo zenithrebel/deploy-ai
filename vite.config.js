f7986565b0d181facefd962ce9b8211f4c1dd2d4
buwono <codervamp@gmail.com>
2025-01-13 19:03:26 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // Fix for SPA routing in development
+  },
+  preview: {
+    historyApiFallback: true, // Fix for SPA routing after build
+  },
+});
