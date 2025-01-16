7758199f2240de11089bfa85dfa17fd8a09565d6
muhammadfathurraiyan <raiyanfathur@gmail.com>
2025-01-16 11:21:34 +0700

    fix(background): change background videoo to image

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,11 +3,13 @@ import { Outlet } from "react-router";
 import { BackgroundVideo } from "../components/layout/main-layout/BackgroundVideo";
 import { Sidebar } from "../components/layout/main-layout/Sidebar";
 import { Header } from "../components/layout/main-layout/Header";
+import { BackgroundImage } from "../components/layout/main-layout/BackgroundImage";
 
 export default function MainLayout() {
   return (
     <div className="relative flex gap-4 h-screen w-full">
-      <BackgroundVideo />
+      {/* <BackgroundVideo /> */}
+      <BackgroundImage />
       <Sidebar />
       <div className="relative z-10 flex flex-col gap-4 lg:py-9 lg:pr-9 py-4 pr-4 max-lg:px-4 size-full">
         <Header />
